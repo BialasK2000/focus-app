05505bc9976dec39180e11970f13422d55a2ff06
Konrad Bialas <platypus1333@gmail.com>
2022-03-20 18:09:20 +0100

    added a 'Task' model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=69)
+    description = models.TextField(max_length=1337)
+    deadline = models.DateTimeField()
+    status = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['deadline']
+        
