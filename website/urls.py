9b619a0a37b76628baa7d625dc09076f790f0a91
Konrad Bialas <platypus1333@gmail.com>
2022-03-23 12:10:27 +0100

    added deleteview

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('focus-details/<int:pk>/', views.DetailTaskView.as_view(), name='detail-tasks'),
     path('focus-create/', views.CreateTaskView.as_view(), name='create-tasks'),
     path('focus-update/<int:pk>/', views.TaskUpdateView.as_view(), name='update-tasks'),
+    path('focus-delete/<int:pk>/', views.TaskDeleteView.as_view(), name='delete-tasks'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -29,4 +29,10 @@ class TaskUpdateView(UpdateView):
     model = Task
     fields = '__all__'
     template_name = "createtasks.html"
+    success_url = reverse_lazy('main-tasks')
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "deletetasks.html"
     success_url = reverse_lazy('main-tasks')
