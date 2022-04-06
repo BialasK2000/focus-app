6a7471d6be59ca1ffcf0d14fd0847f197614722d
Konrad Bialas <platypus1333@gmail.com>
2022-04-06 17:22:15 +0200

    added completed-tasks tab

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('logout/', views.BetaLogoutView.as_view(), name='logout'),
     path('register/', views.BetaRegisterView.as_view(), name='register'),
     path('', views.MainTasksView.as_view(), name='main-tasks'),
+    path('completed-tasks/', views.DeletedTasksView.as_view(), name='completed-tasks'),
     path('focus-details/<int:pk>/', views.DetailTaskView.as_view(), name='detail-tasks'),
     path('focus-create/', views.CreateTaskView.as_view(), name='create-tasks'),
     path('focus-update/<int:pk>/', views.TaskUpdateView.as_view(), name='update-tasks'),
--- website/views.py
+++ website/views.py
@@ -124,3 +124,22 @@ class TaskInvisibleView(LoginRequiredMixin, UpdateView):
     form_class = VisibilityForm
     template_name = "invisibletask.html"
     success_url = reverse_lazy('main-tasks')
+
+
+class DeletedTasksView(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "completedlist.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(status=False).count()
+
+        search_input = self.request.GET.get('search_bar') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(name__icontains=search_input)
+
+        context['search_input'] = search_input
+
+        return context
