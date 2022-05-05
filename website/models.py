9bf323b284d5a49bbcaf01a3ed599674b7fa2108
Konrad Bialas <platypus1333@gmail.com>
2022-05-05 12:33:17 +0200

    fixed csv background

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class Task(models.Model):
     name = models.CharField(max_length=69)
     description = models.TextField(max_length=1337)
     deadline = models.DateField()
-    status = models.BooleanField()
+    status = models.BooleanField("I am currently doing this")
     visible = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
